daf4238047a970ce0946f253e9253ce1b43e76df
2017PGCACA01 <ankitmondal.nitjsr@gmail.com>
2025-05-24 19:41:54 +0530

    feat(ec2): added output and variables file

---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,29 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2 instances"
+  type        = string
+}
+
+variable "user_data" {
+  description = "User data script to initialize EC2 instances"
+  type        = string
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
